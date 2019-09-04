215a49be3a3b26fafa5c932b203a0efdf585b41e
Bruno Nicholas <sbnibro256@gmail.com>
2019-09-04 18:10:45 +0300

    adding the new references

--- app/Models/Reference.php
+++ app/Models/Reference.php
@@ -1,10 +1,52 @@
 <?php
 
 namespace App\Models;
+use App\Models\Department;
+use App\User;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Reference extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'topic',
+        'item',
+        'attachment',
+        'description',
+        'department_id',
+        'status'
+    ];
+
+    /**
+     * The string variable is for the table.
+     *
+     * @var array
+     */
+    protected $table = 'references';
+    
+    /**
+     * Belonds to relationship connects both 
+     * the department table and the departments table
+     *
+     */
+    public function departments()
+    {
+        return $this->belongsTo(Department::class);
+    }
+    
+    /**
+     * Belonds to relationship connects both 
+     * the user table and the departments table
+     *
+     */
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2019_09_04_141622_create_references_table.php
+++ database/migrations/2019_09_04_141622_create_references_table.php
@@ -15,7 +15,17 @@ class CreateReferencesTable extends Migration
     {
         Schema::create('references', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('department_id')->unsigned();
+            $table->string('topic');
+            $table->string('item')->nullable();
+            $table->string('attachment')->nullable();
+            $table->text('description')->nullable();
+            $table->string('status')->->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
         });
     }
 
